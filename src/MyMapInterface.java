4cf7cd4c3dcb2aec6fe21cc713d5447a947d4139
Selftishz <art2sys048@gmail.com>
2022-12-25 21:22:06 +0300

    dadadad

--- src/MyMapInterface.java
+++ src/MyMapInterface.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class MyMapInterface {
+public interface MyMapInterface<k,v> {
+    int size();
+
+    boolean isEmpty();
+
+    Object get(Object key);
+
+    Object put(Object key, Object value);
+
+    interface Entry<k, v> {
+        k getKey();
+
+        v getValue();
+    }
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,5 +1,13 @@
-public class Main {
+
+public class Tester {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyHashMap myHashMap = new MyHashMap();
+        for (int i = 0; i < 100; i++) {
+            myHashMap.put("" + i, "value" + i);
+
+        }
+        for (int i = 0; i < 100; i++) {
+            System.out.println(myHashMap.get("" + i));
+        }
     }
 }
